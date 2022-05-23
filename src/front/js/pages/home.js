64a1d45ad40085bcfe0cd7cf684c495a421be009
ChickenOTC <alekreed98@gmail.com>
2022-05-23 22:47:24 +0000

    added text

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,7 +9,7 @@ export const Home = () => {
   return (
     <div className="text-center mt-5">
       <h1>Hello Rigo!!</h1>
-      <p>Getting Started</p>
+      <p>Getting Started, lets go</p>
     </div>
   );
 };

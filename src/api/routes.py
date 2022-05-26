05d9bf5fb03a34e1a6c3f8e29c97b804db706676
Aussymo <aussymo@gmail.com>
2022-05-26 00:18:28 +0000

    added routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,18 +1,42 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return jsonify({"Message": "User not found"}), 401
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+    print(access_token)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route("/user/active", methods=["POST"])
+def active_user():
+    print("Active user")
+    body=request.get_json()
+    one_user = User.query.filter_by(email=body["email"]).first()
+    return jsonify(one_user.serialize()), 200
 
-    return jsonify(response_body), 200
+@api.route("/user", methods=["POST"])
+def create_user():
+    body = request.get_json()
+    email = body["email"]
+    password = body["password"]
+    username = body["username"]
+    phone_number = body["phone_number"]
+    user = User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit(user)
+    
+    return (jsonify(user.serialize))
